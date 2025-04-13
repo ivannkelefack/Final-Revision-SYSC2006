a463abadfc022e670fd10bb5bd4db188ffe44c7c
ivan <ivan-nkelefack@outlook.com>
2025-04-13 12:47:30 -0400

    update of peek function

--- stack_exercices.c
+++ stack_exercices.c
@@ -1,60 +1,61 @@
 
 #include <stdio.h>
 #include <assert.h>
-#include <stdlib.h> // For malloc and free
+#include <stdlib.h>  // For malloc and free
 #include <stdbool.h> // For boolean types
 #include <stddef.h>  // For size_t
 #include <string.h>  // For memset
-
 
 #include "functions.h"
 
 // Implement your stack-related functions here
 
-int main() {
+int main()
+{
     // Test your stack functions here
     printf("Stack exercises initialized.\n");
     return 0;
 }
 
-void initializeStack(Stack* stack){
-    struct Stack *stack = malloc(sizeof(Stack)); 
-    assert(Stack!=NULL); 
+void initializeStack(Stack *stack)
+{
+    struct Stack *stack = malloc(sizeof(Stack));
+    assert(Stack != NULL);
 
-    stack->top = NULL; 
-    stack->size = 0; 
-
+    stack->top = NULL;
+    stack->size = 0;
 }
 
-bool isStackEmpty(const Stack* stack){
-    struct Stack *stack = malloc(sizeof(Stack)); 
-    assert(Stack!=NULL); 
+bool isStackEmpty(const Stack *stack)
+{
+    struct Stack *stack = malloc(sizeof(Stack));
+    assert(Stack != NULL);
 
-    if ( stack->size == 0 && stack->top == NULL)
+    if (stack->size == 0 && stack->top == NULL)
     {
-        return true; 
+        return true;
     }
 
-    return false; 
-
+    return false;
 }
 
-void push(Stack* stack, int data){
-    assert(stack != NULL); 
-    struct StackNode *newnode = malloc(sizeof(StackNode)); 
-    assert(StackNode!=NULL);
-    newnode->data = data; 
-    newnode->next = stack->top; 
-    stack->top = newnode; 
-    stack->size += 1; 
-
+void push(Stack *stack, int data)
+{
+    assert(stack != NULL);
+    struct StackNode *newnode = malloc(sizeof(StackNode));
+    assert(StackNode != NULL);
+    newnode->data = data;
+    newnode->next = stack->top;
+    stack->top = newnode;
+    stack->size += 1;
 }
 
-_Bool pop(Stack* stack, int *elem){
-    assert(stack != NULL); 
-    if ( stack->size == 0 && stack->top == NULL)
+_Bool pop(Stack *stack, int *elem)
+{
+    assert(stack != NULL);
+    if (stack->size == 0 && stack->top == NULL)
     {
-        return false; 
+        return false;
     }
 
     *elem = stack->top->data;
@@ -63,10 +64,15 @@ _Bool pop(Stack* stack, int *elem){
     free(to_remove);
     stack->size -= 1;
     return true;
-    
 }
 
-_Bool peek(Stack* stack, int* elem){
-
+_Bool peek(Stack *stack, int *elem)
+{
+    assert(stack != NULL);
+    if (stack->size == 0)
+    {
+        return false;
+    }
+    *elem = stack->top->element;
+    return true;
 }
-
